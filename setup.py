45b522816cb4fd156f3c92db1e507fa67865f66c
Mai Anh Vu <maianh@Mais-MacBook-Pro.local>
2023-04-23 15:07:02 +0700

    [a] adding setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='dimv_imputation',
+      version= '0.1',
+      description= 'Missing data imputation with Bayesian Conditional Expectation',
+      url= 'http://github.com/maianhpuco/DIMV_imputation',
+      author= '(1) Mai Anh Vu (2) Thu Nguyen',
+      author_email= 'maianhuel@gmail.com',
+      packages= find_packages(),
+      zip_safe=False)
