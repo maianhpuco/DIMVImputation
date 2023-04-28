403f6bd20888c73fba15339bfea004b5e4b633b4
Mai Anh Vu <maianh@Mais-MacBook-Pro.local>
2023-04-28 14:35:47 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(name='DIMVImputation',
       version= '0.0.1',
       description= 'Conditional Expectation for missing data imputation ',
-      url= 'http://github.com/maianhpuco/DIMV_imputation',
+      url= '',
       author= ''
       author_email= '',
-      package_dir={"":"dimv_imputation"}
+      package_dir={"":"src"}
       packages= find_packages(),
       zip_safe=False)
+
