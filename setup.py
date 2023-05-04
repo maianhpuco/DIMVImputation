4a704abc0119cb7187d7232ac2f504409c8f4531
Mai Anh Vu <maianh@Mais-MacBook-Pro.local>
2023-05-04 14:48:08 +0700

    adding set up file

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
 from setuptools import setup, find_packages
 
-setup(name='DIMVImputation',
-      version= '0.0.1',
-      description= 'Conditional Expectation for missing data imputation ',
-      url= '',
-      author= ''
-      author_email= '',
-      package_dir={"":"src"}
-      packages= find_packages(),
-      zip_safe=False)
-
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines() 
+    
+setup(
+    name='my_package',
+    version='0.1.0',
+    description='My awesome Python package',
+    url='https://github.com/maianh.puco/DIMVImputation.git',
+#     author='Your Name',
+#     author_email='your@email.com',
+    packages=find_packages(),
+    install_requires=requirements,
+#     classifiers=[
+#         'Development Status :: 3 - Alpha',
+#         'Intended Audience :: Developers',
+#         'Topic :: Software Development :: Libraries',
+#         'License :: OSI Approved :: MIT License',
+#         'Programming Language :: Python :: 3',
+#         'Programming Language :: Python :: 3.6',
+#         'Programming Language :: Python :: 3.7',
+#         'Programming Language :: Python :: 3.8',
+#         'Programming Language :: Python :: 3.9',
+#     ],
+    keywords='python package',
+    python_requires='>=3.6',
+) 
