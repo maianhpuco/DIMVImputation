fd10bc6508488596d4f559455aec0fe648a77bee
Mai Anh Vu <maianhuel@gmail.com>
2023-05-22 22:31:53 +0700

    add setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='DIMVImputation',
+    version='0.1.0',
+    description='My awesome DIMVImputation package',
+    url='https://github.com/maianh.puco/DIMVImputation.git',
+    #     author='Your Name',
+    #     author_email='your@email.com',
+    #    packages=find_packages(),
+    package_dir={"": "src"},
+    install_requires=requirements,
+    #     classifiers=[
+    #         'Development Status :: 3 - Alpha',
+    #         'Intended Audience :: Developers',
+    #         'Topic :: Software Development :: Libraries',
+    #         'License :: OSI Approved :: MIT License',
+    #         'Programming Language :: Python :: 3',
+    #         'Programming Language :: Python :: 3.6',
+    #         'Programming Language :: Python :: 3.7',
+    #         'Programming Language :: Python :: 3.8',
+    #         'Programming Language :: Python :: 3.9',
+    #     ],
+    keywords='python package',
+    python_requires='>=3.6',
+)
