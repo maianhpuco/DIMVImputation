a72c5d6fd2d49bb5cd36946b4784b9d261ed798f
Tu T. Do <tu.dothanh1906@gmail.com>
2023-05-23 13:46:19 +0700

    Done exp

--- src/DIMVImputation.py
+++ src/DIMVImputation.py
@@ -1,16 +1,17 @@
-import numpy as np 
-from tqdm import tqdm 
-from typing import * 
-from dpers import DPERS 
-from conditional_expectation import RegularizedConditionalExpectation 
 import numpy as np
+from tqdm import tqdm
+from typing import List, Dict, Union
+from dpers import DPERS
+from conditional_expectation import RegularizedConditionalExpectation
 from utils import normalize, find_largest_elements, rmse_loss, rescale
 
+
 class DIMVImputation:
-    """ Imputation class that use conditional expectation to fill the missing data position
-        The covariance matrix would be computed by the DPER algorithm  
+    """Imputation class that use conditional expectation to fill the missing data position
+    The covariance matrix would be computed by the DPER algorithm
     """
-    def __init__(self):
+
+    def __init__(self, complete_case: bool):
         self.initializing = False
         self.Xtrain = None
         self.X_train_norm = None
@@ -22,40 +23,56 @@ class DIMVImputation:
         self.estimator = None
         self.best_alpha = None
         self.cv_score = None
-        
-    def fit(self, X: np.ndarray, initializing: bool= False, n_jobs = None) -> None:
+
+        # If complete_case is True, use complete case covariant
+        # Otherwise use dpers
+        self.complete_case = complete_case
+
+    def fit(self, X: np.ndarray, initializing: bool = False, n_jobs=None) -> None:
         """
-        Imputation class that use conditional expectation to fill the missing data position
-        The covariance matrix would be computed by the DPER algorithm 
-        When to use initializing : Initialize = True should be set if there is many missing pattern in the dataset for example randomly missing
+        Imputation class that use conditional expectation to fill the missing
+        data position. The covariance matrix would be computed by the DPER algorithm
+        When to use initializing : Initialize = True should be set if there is many
+        missing pattern in the dataset for example randomly missing
         Args:
-            X (np.ndarray): Xtrain, dataset used to computer the covariance matrix 
-            initializing (bool, optional): Defaults to False; when  initializing is set as True, the missing position would be all initialize by 0 (also the mean of the scaled)
-            n_jobs : umber of parallel jobs to run the covariance computation 
+            X (np.ndarray): Xtrain, dataset used to computer the covariance matrix
+            initializing (bool, optional):
+                Defaults to False;
+                when  initializing is set as True, the missing position would
+                be all initialize by 0 (also the mean of the scaled)
+            n_jobs : umber of parallel jobs to run the covariance computation
         """
-        self.initializing = initializing 
+        self.initializing = initializing
         self.Xtrain = X
         self.X_train_norm, self.train_mean, self.train_std = normalize(X, X)
 
-        self.cov = DPERS().fit(self.X_train_norm, n_jobs = n_jobs)
-        self.no_0_var_mask = np.diag(self.cov)!=0
+        if self.complete_case:
+            # index of complete observation
+            idx = ~np.isnan(X).any(-1)
+            X_complete = X[idx, :]
+
+            # Compute covariance using X_complete
+            self.cov = np.cov(X_complete, rowvar=False)
+        else:
+            self.cov = DPERS().fit(self.X_train_norm, n_jobs=n_jobs)
+
+        self.no_0_var_mask = np.diag(self.cov) != 0
 
         self.cov_no_zeros = self.cov[self.no_0_var_mask, :][:, self.no_0_var_mask]
 
         self.estimator = RegularizedConditionalExpectation(
-                cov = self.cov_no_zeros,
-                initializing = self.initializing) 
-        self.best_alpha = None 
-        self.cv_score = None 
+            cov=self.cov_no_zeros, initializing=self.initializing
+        )
+        self.best_alpha = None
+        self.cv_score = None
         self.cv_mode = False
-        
 
     def cross_validate(
         self,
-        alphas: List[float]=None,
-        train_percent: float=None,
+        alphas: List[float] = None,
+        train_percent: float = None,
         features_corr_threshold: float = 0,
-        mlargest_features: int = 1
+        mlargest_features: int = 1,
     ) -> Dict[str, Union[float, List[float]]]:
         """
         Perform cross-validation on the model.
@@ -69,127 +86,131 @@ class DIMVImputation:
         Returns:
             Dict[str, Union[float, List[float]]]: {"best_alpha" :best_alpha, "cv_scores": scores}  Results of cross-validation.
         """
-        #set cv_mode = True
-        self.cv_mode = True 
+        # set cv_mode = True
+        self.cv_mode = True
 
         if train_percent is None:
-            train_percent = 1.0 
+            train_percent = 1.0
 
-        if  alphas is None: 
-             alphas = [0.0, 0.01, 0.1, 1.0, 10.0, 100.0]
+        if alphas is None:
+            alphas = [0.0, 0.01, 0.1, 1.0, 10.0, 100.0]
 
-        print("Start Cross Validation with alphas = {} and {} % of training set".format(alphas, train_percent*100))
-        assert (train_percent <= 1 and train_percent > 0.05), " train_percent must be in range(0.1, 1] "
+        print(
+            "Start Cross Validation with alphas = {} and {} % of training set".format(
+                alphas, train_percent * 100
+            )
+        )
+        assert (
+            train_percent <= 1 and train_percent > 0.05
+        ), " train_percent must be in range(0.1, 1] "
 
         best_score = np.inf
         best_alpha = None
 
         Xtrain = self.Xtrain
-        
+
         scores = {}
         n = self.Xtrain.shape[0]
-        
+
         shuffle_idxes = np.arange(n)
         np.random.shuffle(shuffle_idxes)
-        sample_idxes  = shuffle_idxes[: int(n * train_percent)]
-        
+        sample_idxes = shuffle_idxes[: int(n * train_percent)]
+
         X_cv = self.Xtrain[sample_idxes, :]
-        
+
         observed_mask = ~np.isnan(X_cv)
 
         for alpha in alphas:
-            print("Running Cross Validation, alpha={}".format(alpha)) 
+            print("Running Cross Validation, alpha={}".format(alpha))
 
             try:
                 X_cv_imputed = self.transform(
-                    X_cv, 
+                    X_cv,
                     alpha=alpha,
-                    features_corr_threshold=features_corr_threshold, 
-                    mlargest_features=mlargest_features)
-                    
+                    features_corr_threshold=features_corr_threshold,
+                    mlargest_features=mlargest_features,
+                )
+
                 # print(X_cv_imputed.shape)
 
-                score = rmse_loss(X_cv, X_cv_imputed) 
-                #X_cv contain nan value, only computer score at not NaN position (not using initialize as 0 position when initialized=True)
-                #print("RMSE at alpha {} is {}".format(alpha, score))
+                score = rmse_loss(X_cv, X_cv_imputed)
+                # X_cv contain nan value, only computer score at not NaN position (not using initialize as 0 position when initialized=True)
+                # print("RMSE at alpha {} is {}".format(alpha, score))
 
                 scores.update({alpha: score})
 
                 if score < best_score:
                     best_score = score
-                    best_alpha = alpha 
+                    best_alpha = alpha
 
             except Exception as err:
                 print(f"Unexpected {err=}, {type(err)=}")
                 raise
-        
+
         self.best_alpha = best_alpha
-        self.cv_score = scores  
-        results = {"best_alpha" :best_alpha, "cv_scores": scores} 
-        #turn off the cross-validation mode: cv_mode 
-        self.cv_mode = False 
-        return best_alpha 
+        self.cv_score = scores
+        results = {"best_alpha": best_alpha, "cv_scores": scores}
+        # turn off the cross-validation mode: cv_mode
+        self.cv_mode = False
+        return best_alpha
 
     def filter_features(
-        self, 
+        self,
         s_missing_fts: np.array,
-        s_avai_fts: np.array, 
-        idx: int,  
+        s_avai_fts: np.array,
+        idx: int,
         th: float = 0,
         mlargest: int = None,
-        ):
+    ):
         """
         Choosing a set of feature that correlation with the target feature f (imputed features) with criteria:
             1. Have correlation equal to or greater than a threshold features_corr_threshold
             2. If no feature meet the condition, choose top m features that have highest correlation with feature f
-        
-        Args: 
+
+        Args:
             s_missing_fts (numpy.array): a boolean array indicating which features are missing for the current instance
             s_avai_fts (numpy.array): a boolean array indicating which features are available for the current instance
-            idx (int): the index of the target feature to be imputed 
+            idx (int): the index of the target feature to be imputed
             th (float, optional): the threshold for selecting features with correlation to the target feature. Default value is 0. (mean all the feature in `s_avai_fts`)
             mlargest (int, optional): the maximum number of features to select if no feature meets the correlation threshold. Default value is None, which indicates that all available features should be considered.
-        
+
         Returns:
-            new_s_avai_fts (numpy.array): a boolean array indicating which features have been selected for imputation, based on the specified criteria. 
+            new_s_avai_fts (numpy.array): a boolean array indicating which features have been selected for imputation, based on the specified criteria.
         """
 
-        mlargest =  mlargest + 1
-        S = self.cov_no_zeros 
+        mlargest = mlargest + 1
+        S = self.cov_no_zeros
 
-        threshold_filter = abs(self.cov_no_zeros[idx, :]) >= th 
+        threshold_filter = abs(self.cov_no_zeros[idx, :]) >= th
         mlargest_filter = find_largest_elements(
-            s_missing_fts, 
-            s_avai_fts, 
-            self.cov_no_zeros[idx, :].copy(), 
-            mlargest) 
-        
-        new_s_avai_fts = np.zeros_like(self.cov_no_zeros[idx, :], dtype=bool) 
+            s_missing_fts, s_avai_fts, self.cov_no_zeros[idx, :].copy(), mlargest
+        )
 
-        if np.sum(threshold_filter) > 1:             
-            new_s_missing_fts  =  s_missing_fts   
-            new_s_avai_fts     =  s_avai_fts * threshold_filter
-        
-        if np.sum(new_s_avai_fts) == 0: 
-            new_s_avai_fts     =  s_avai_fts * mlargest_filter
+        new_s_avai_fts = np.zeros_like(self.cov_no_zeros[idx, :], dtype=bool)
+
+        if np.sum(threshold_filter) > 1:
+            new_s_missing_fts = s_missing_fts
+            new_s_avai_fts = s_avai_fts * threshold_filter
+
+        if np.sum(new_s_avai_fts) == 0:
+            new_s_avai_fts = s_avai_fts * mlargest_filter
             n = self.cov_no_zeros.shape[0]
             tmp = np.arange(n)
 
-        return new_s_avai_fts 
+        return new_s_avai_fts
 
     def transform(
-            self, 
-            X_input: np.ndarray, 
-            alpha: np.ndarray = 0,
-            #run_cross_validation: bool = True,
-            #train_percent_cv: float = 1.0,
-            #alphas_cv: List = None,
-            features_corr_threshold = None, 
-            mlargest_features = None 
-            ) -> np.ndarray:  
-
-        """ 
-        Imputes the estimated value for missing position in the input array X_input using the covariance matrix calculated in the fit step 
+        self,
+        X_input: np.ndarray,
+        alpha: np.ndarray = 0,
+        # run_cross_validation: bool = True,
+        # train_percent_cv: float = 1.0,
+        # alphas_cv: List = None,
+        features_corr_threshold=None,
+        mlargest_features=None,
+    ) -> np.ndarray:
+        """
+        Imputes the estimated value for missing position in the input array X_input using the covariance matrix calculated in the fit step
         Args:
             X_input (np.ndarray): Input array of shape (n_samples, n_features) containing missing values
             alpha (np.ndarray, optional): Array of shape (n_features,) containing the regularization parameter for each feature
@@ -202,98 +223,100 @@ class DIMVImputation:
             np.ndarray: Imputed array of shape (n_samples, n_features)
         """
         X_input = X_input.astype(np.float64)
-        
+
         if mlargest_features is not None:
-            assert mlargest_features <= self.cov_no_zeros.shape[0], \
-                "Value of features_corr_threshold should be smaller than number of features that have " 
-            assert features_corr_threshold is not None, \
-                "mlargest_features is the m features to filter if the condition features_corr_threshold is not meet, please config the features_corr_threshold's value" 
-        
-        if features_corr_threshold is not None: 
-            assert 0 <= features_corr_threshold <= 1, \
-                "Value must be between 0 and 1"  
+            assert (
+                mlargest_features <= self.cov_no_zeros.shape[0]
+            ), "Value of features_corr_threshold should be smaller than number of features that have "
+            assert (
+                features_corr_threshold is not None
+            ), "mlargest_features is the m features to filter if the condition features_corr_threshold is not meet, please config the features_corr_threshold's value"
+
+        if features_corr_threshold is not None:
+            assert 0 <= features_corr_threshold <= 1, "Value must be between 0 and 1"
             if mlargest_features is None:
                 mlargest_features = 1
 
-        #scaling by mean and std of train set 
+        # scaling by mean and std of train set
         X_test_norm, _, _ = normalize(X_input, mean=self.train_mean, std=self.train_std)
-        
-        X            = X_test_norm[:, self.no_0_var_mask]
+
+        X = X_test_norm[:, self.no_0_var_mask]
         missing_data = X_test_norm[:, self.no_0_var_mask]
 
         if self.initializing == True:
-            X[np.isnan(X)] = 0  
+            X[np.isnan(X)] = 0
 
         missing_ftss = np.where(np.isnan(missing_data).any(axis=0))[0]
 
-        X_imp_normed = np.zeros_like(X, dtype='float') 
-    
-        #check cross_validation only exist if initialize = True 
+        X_imp_normed = np.zeros_like(X, dtype="float")
 
-        if self.initializing==True:
+        # check cross_validation only exist if initialize = True
+
+        if self.initializing == True:
             X[np.isnan(X)] == 0
 
         S = self.cov_no_zeros
 
         for idx in tqdm(missing_ftss):
-            
             if self.initializing == True or self.cv_mode == True:
                 all_values = np.arange(X[:, idx].shape[0])
                 missing_values = all_values
-            else: 
-                missing_values = np.where(np.isnan(X[:, idx]) != 0)[0] 
-             
-            while len(missing_values) > 0 : 
-                s = missing_values[0]
-                #reduce feature set
+            else:
+                missing_values = np.where(np.isnan(X[:, idx]) != 0)[0]
 
-                s_missing_fts = np.isnan(X[s,:]) 
-                s_avai_fts    = ~np.isnan(X[s,:])
+            while len(missing_values) > 0:
+                s = missing_values[0]
+                # reduce feature set
+
+                s_missing_fts = np.isnan(X[s, :])
+                s_avai_fts = ~np.isnan(X[s, :])
                 # import pdb; pdb.set_trace()
-                if features_corr_threshold is not None: 
+                if features_corr_threshold is not None:
                     s_avai_fts = self.filter_features(
-                        s_missing_fts, 
+                        s_missing_fts,
                         s_avai_fts,
                         idx,
-                        th = features_corr_threshold,
-                        mlargest = mlargest_features
-                        ) 
-                    # print("Ture") 
-                    
-                # print("s_avai_fts", np.sum(s_avai_fts))
-                
-                same_missing_fts_mask = np.sum(~np.isnan(X[:, s_missing_fts]), axis=1) == 0 
-                same_avai_fts_mask    = np.sum(np.isnan(X[:, s_avai_fts]), axis=1) == 0
-                same_missing_pattern  = same_missing_fts_mask * same_avai_fts_mask
+                        th=features_corr_threshold,
+                        mlargest=mlargest_features,
+                    )
+                    # print("Ture")
 
-                s_missing_fts[idx] = False 
-                s_avai_fts[idx] = False  
+                # print("s_avai_fts", np.sum(s_avai_fts))
+
+                same_missing_fts_mask = (
+                    np.sum(~np.isnan(X[:, s_missing_fts]), axis=1) == 0
+                )
+                same_avai_fts_mask = np.sum(np.isnan(X[:, s_avai_fts]), axis=1) == 0
+                same_missing_pattern = same_missing_fts_mask * same_avai_fts_mask
+
+                s_missing_fts[idx] = False
+                s_avai_fts[idx] = False
 
                 pred = self.estimator.transform(
-                    feature_idxes = s_avai_fts, 
-                    label_idx = idx, 
-                    rows = same_missing_pattern, 
-                    missing_data = missing_data, 
-                    alpha = alpha
-                    )
-                
+                    feature_idxes=s_avai_fts,
+                    label_idx=idx,
+                    rows=same_missing_pattern,
+                    missing_data=missing_data,
+                    alpha=alpha,
+                )
+
                 X_imp_normed[same_missing_pattern, idx] = pred
 
-                #pop from the queue 
+                # pop from the queue
                 pop_idxs = np.where(same_missing_pattern)[0]
                 missing_values = missing_values[~np.in1d(missing_values, pop_idxs)]
-        
-        #rescaled the imputed result to the original scale
+
+        # rescaled the imputed result to the original scale
         Xoutput = X_test_norm.copy()
         Xoutput[:, self.no_0_var_mask] = X_imp_normed
-        Xoutput[np.isnan(Xoutput)] = 0 
+        Xoutput[np.isnan(Xoutput)] = 0
 
-        #rescaled by mean and std of train set 
-        X_imp_rescaled = rescale(Xoutput, self.train_mean, self.train_std) 
+        # rescaled by mean and std of train set
+        X_imp_rescaled = rescale(Xoutput, self.train_mean, self.train_std)
 
         Ximp = X_input.copy()
         Ximp[np.isnan(X_input)] = X_imp_rescaled[np.isnan(X_input)]
-        
+
         if self.cv_mode == True:
             Ximp_cv_mode = X_imp_rescaled
             return Ximp_cv_mode
